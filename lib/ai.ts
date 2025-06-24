6f4b89ed025662c4218147d8d8c3acba6a716ad4
ItsRudraksh <122355077+ItsRudraksh@users.noreply.github.com>
2025-06-25 04:22:18 +0530

    Updated ai.ts

--- lib/ai.ts
+++ lib/ai.ts
@@ -3,6 +3,7 @@ import OpenAI from "openai";
 const client = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENROUTER_API_KEY || "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",
+  dangerouslyAllowBrowser:true,
 });
 
 export interface ChatMessage {
